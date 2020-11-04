f048a8354cf270f853e687561864f7bb3c1f38b3
Wojciech Melone <w.melone97@gmail.com>
2020-11-04 16:13:12 +0100

    Deploy demo app (create build folder)

--- frontend/src/theme/mainTheme.js
+++ frontend/src/theme/mainTheme.js
@@ -1,16 +1,18 @@
-export const theme = {
-  grey100: "hsl(0, 0%, 96%)",
-  grey200: "hsl(0, 0%, 90%)",
-  grey300: "hsl(0, 0%, 70%)",
-  black: "hsl(0, 0%, 0%)",
+const theme = {
+  grey100: 'hsl(0, 0%, 96%)',
+  grey200: 'hsl(0, 0%, 90%)',
+  grey300: 'hsl(0, 0%, 70%)',
+  black: 'hsl(0, 0%, 0%)',
   light: 400,
   bold: 700,
   fontSize: {
-    xxs: "1rem",
-    xs: "1.2rem",
-    s: "1.6rem",
-    m: "2.1rem",
-    l: "2.4rem",
-    xl: "4rem",
+    xxs: '1rem',
+    xs: '1.2rem',
+    s: '1.6rem',
+    m: '2.1rem',
+    l: '2.4rem',
+    xl: '4rem',
   },
 };
+
+export default theme;
--- frontend/src/views/Root.js
+++ frontend/src/views/Root.js
@@ -1,11 +1,11 @@
-import React from "react";
-import { BrowserRouter } from "react-router-dom";
-import Navigation from "../components/organisms/Navigation/Navigation";
-import GlobalStyle from "../theme/GlobalStyle";
-import { theme } from "../theme/mainTheme";
-import Home from "./Home";
-import Heading from "../components/atoms/Heading/Heading";
-import Footer from "../components/organisms/Footer/Footer";
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Navigation from '../components/organisms/Navigation/Navigation';
+import GlobalStyle from '../theme/GlobalStyle';
+import theme from '../theme/mainTheme';
+import Home from './Home';
+import Heading from '../components/atoms/Heading/Heading';
+import Footer from '../components/organisms/Footer/Footer';
 
 const Root = () => (
   <BrowserRouter theme={theme}>
