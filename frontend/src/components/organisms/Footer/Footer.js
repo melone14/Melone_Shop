e82c7ed3cac6d1178d6a0625c10442a3ef5cbf31
Wojciech Melone <w.melone97@gmail.com>
2020-11-06 18:26:39 +0100

    Add footer changes

--- frontend/src/components/organisms/Footer/Footer.js
+++ frontend/src/components/organisms/Footer/Footer.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 import Accordion from './Accordion';
 import { theCompany, findUsOn } from './links';
+import logo from '../../../assets/icons/logo.png';
 
 const Wrapper = styled.footer`
   background: #1b1b1b;
@@ -20,14 +22,29 @@ const Content = styled.div`
 `;
 
 const StyledRights = styled.p`
-  color: gray;
+  color: #1b1b1b;
   display: block;
   margin: 0 auto;
   font-size: 1.3rem;
   text-align: center;
-  @media (max-width: 42.5rem) {
-    margin: 0;
-  }
+  padding-bottom: 1.5rem;
+`;
+
+const LogoWrapper = styled.div`
+  background-color: #dedfe0;
+  height: 20vh;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const StyledLogo = styled(Link)`
+  display: block;
+  width: 45%;
+  height: 100%;
+  background: url(${logo}) center/cover no-repeat border-box;
+  background-size: 100%;
 `;
 
 const Footer = () => (
@@ -37,8 +54,11 @@ const Footer = () => (
         <Accordion heading="The Company" links={theCompany} />
         <Accordion heading="About us" links={findUsOn} />
       </Content>
-      <StyledRights>©2020-2022 Melone S.A. - All rights reserved</StyledRights>
     </InnerWrapper>
+    <LogoWrapper>
+      <StyledLogo to="/" />
+      <StyledRights>©2020-2022 Melone S.A. - All rights reserved</StyledRights>
+    </LogoWrapper>
   </Wrapper>
 );
 
