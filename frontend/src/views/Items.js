26b86987598c7d70e616c2605703d537884fee88
Wojciech Melone <w.melone97@gmail.com>
2020-11-12 09:48:30 +0100

    Add heart to the Item

--- frontend/src/views/Items.js
+++ frontend/src/views/Items.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
-import background from '../assets/images/koszulka3.jpg';
+import koszulka1 from '../assets/images/koszulka1.jpg';
+import koszulka2 from '../assets/images/koszulka2.jpg';
+import koszulka3 from '../assets/images/koszulka3.jpg';
+import koszulka4 from '../assets/images/koszulka4.jpg';
+import koszulka5 from '../assets/images/koszulka5.jpg';
+import koszulka6 from '../assets/images/koszulka6.jpg';
+import koszulka7 from '../assets/images/koszulka7.jpg';
+import koszulka8 from '../assets/images/koszulka8.jpg';
+import heart from '../assets/icons/heart.svg';
 
 const Wrapper = styled.section`
   width: 100vw;
@@ -20,19 +28,20 @@ const ItemsWrapper = styled.section`
 `;
 
 const Item = styled.div`
+  position: relative;
   width: 49.5vw;
   height: 35vh;
   margin: 0 auto;
-  background: url(${background}) center/cover no-repeat border-box;
+  background: url(${({ background }) => background}) center/cover no-repeat border-box;
 `;
 
 const Price = styled.p`
-  position: relative;
+  position: absolute;
   display: block;
   width: 70px;
   height: 20px;
   text-align: center;
-  top: 90%;
+  bottom: 2%;
   left: 30%;
   font-weight: 300;
   color: #4b4b4b;
@@ -56,30 +65,38 @@ const FilterBtn = styled.button`
   outline: none;
 `;
 
+const Heart = styled.div`
+  position: absolute;
+  right: 10px;
+  top: 10px;
+  height: 25px;
+  width: 25px;
+  background: url(${heart}) center/cover no-repeat border-box;
+`;
+
+const initialProducts = [
+  { image: koszulka1 },
+  { image: koszulka2 },
+  { image: koszulka3 },
+  { image: koszulka4 },
+  { image: koszulka5 },
+  { image: koszulka6 },
+  { image: koszulka7 },
+  { image: koszulka8 },
+];
+
 const Items = () => {
   return (
     <Wrapper>
       <Heading>T-Shirts</Heading>
       <FilterBtn>Filter by</FilterBtn>
       <ItemsWrapper>
-        <Item>
-          <Price>$ 100</Price>
-        </Item>
-        <Item>
-          <Price>$ 100</Price>
-        </Item>
-        <Item>
-          <Price>$ 100</Price>
-        </Item>
-        <Item>
-          <Price>$ 100</Price>
-        </Item>
-        <Item>
-          <Price>$ 100</Price>
-        </Item>
-        <Item>
-          <Price>$ 100</Price>
-        </Item>
+        {initialProducts.map((item) => (
+          <Item background={item.image}>
+            <Heart />
+            <Price>$ 100</Price>
+          </Item>
+        ))}
       </ItemsWrapper>
     </Wrapper>
   );
